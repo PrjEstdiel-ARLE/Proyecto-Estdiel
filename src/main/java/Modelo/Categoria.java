40758fa04c37d25131b7f33b4c7e897d1b2e4dd3
Andre Lagos <u22214641@utp.edu.pe>
2025-04-26 23:20:28 -0500

    Creacion de carpeta modelo y clase categoría

---
+++ src/main/java/Modelo/Categoria.java
@@ -0,0 +1,45 @@
+package Modelo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCategoria;
+    private String nombre;
+    private double ganancia;
+
+    public Categoria() {
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getGanancia() {
+        return ganancia;
+    }
+
+    public void setGanancia(double ganancia) {
+        this.ganancia = ganancia;
+    }
+
+}
