7d6f138fb57d4287c4e1791410cc029c79b465ca
Andre <u22214641@utp.edu.pe>
2025-06-27 16:14:46 -0500

    CRUD de usuarios listo

--- src/main/java/Controlador/ControladoraRol.java
+++ src/main/java/Controlador/ControladoraRol.java
@@ -6,17 +6,22 @@ import Modelo.Rol;
 import java.util.List;
 
 public class ControladoraRol {
+
     private final RolDAO rolDao;
 
     public ControladoraRol() {
         this.rolDao = new RolDAOImpl();
     }
-    
-    public List<Rol> leerTodos(){
+
+    public List<Rol> leerTodos() {
         return rolDao.leerTodos();
     }
-    
-    public Rol leer(int id){
+
+    public Rol leer(int id) {
         return rolDao.leer(id);
+    }
+
+    public Rol leerPorNombre(String rolNombre) {
+        return rolDao.leerPorNombre(rolNombre);
     }
 }
--- src/main/java/DAO/RolDAO.java
+++ src/main/java/DAO/RolDAO.java
@@ -6,5 +6,6 @@ import java.util.List;
 public interface RolDAO {
     List<Rol> leerTodos();
     Rol leer(int id);
+    public Rol leerPorNombre(String rolNombre);
 }
 
