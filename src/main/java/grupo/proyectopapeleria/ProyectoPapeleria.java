9ef5c565f6b7bb2f88a83951fb63d33247f889c4
Erick <erickantezanatorres@gmail.com>
2025-07-14 22:05:46 -0500

    Cambios

--- src/main/java/grupo/proyectopapeleria/ProyectoPapeleria.java
+++ src/main/java/grupo/proyectopapeleria/ProyectoPapeleria.java
@@ -7,7 +7,8 @@ import Vista.Login;
 
 public class ProyectoPapeleria {
 
-    public static void main(String[] args) {      
+    public static void main(String[] args) {    
+        ControladoraGeneral CG = new ControladoraGeneral();
         Login igu = new Login();
 
         igu.setVisible(true);
